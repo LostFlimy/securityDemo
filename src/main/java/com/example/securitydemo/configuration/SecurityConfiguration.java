8d9d4aed41a6b0e4e68f66e763fcf424aae723eb
dsinitcyn <d.sinitcyn@intabia.ru>
2023-02-17 16:45:12 +0700

    Поправил спринг секьюрити конфиг, теперь вместе с Keycloak провайдером работает и inMemoryAuth

--- src/main/java/com/example/securitydemo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/securitydemo/configuration/SecurityConfiguration.java
@@ -1,14 +1,12 @@
 package com.example.securitydemo.configuration;
 
 import java.util.Arrays;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -19,40 +17,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-  @Override
-  @Order(1)
-  protected void configure (AuthenticationManagerBuilder http) throws Exception {
-   http.authenticationProvider(keycloakAuthenticationProvider());
-  }
-
-  @Order(2)
-  protected void configureGlobal(AuthenticationManagerBuilder http) throws Exception {
-    http.userDetailsService(users()).and().authenticationProvider();
-  }
-
-  @Bean public AuthenticationManager authenticationManager() {
-    return new ProviderManager(Arrays.asList(keycloakAuthenticationProvider()));
-  }
-
-  @Bean public KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
-    return new KeycloakAuthenticationProvider();
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-        .cors().and()
-        .authorizeRequests()
-        .antMatchers("/hello").hasRole("USER")
-        .antMatchers("/hi").permitAll()
-        .antMatchers("/configure").hasRole("ADMIN")
-        .and()
-        .formLogin().successForwardUrl("/hello");
-  }
-
   @Bean
-  public UserDetailsService users() {
+  public UserDetailsService userDetailsService() {
     UserDetails admin = User.withDefaultPasswordEncoder()
         .username("admin")
         .password("password")
@@ -67,4 +33,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     return new InMemoryUserDetailsManager(user, admin);
   }
 
+  @Bean public AuthenticationManager authenticationManager() {
+    return new ProviderManager(Arrays.asList(keycloakAuthenticationProvider(), daoAuthenticationProvider()));
+  }
+
+  @Bean public KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
+    return new KeycloakAuthenticationProvider();
+  }
+
+  @Bean public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsService());
+    return provider;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().userDetailsService(userDetailsService())
+        .cors().and()
+        .authorizeRequests()
+        .antMatchers("/hello").hasRole("USER")
+        .antMatchers("/hi").permitAll()
+        .antMatchers("/configure").hasRole("ADMIN")
+        .and()
+        .formLogin().defaultSuccessUrl("/hello");
+  }
 }
